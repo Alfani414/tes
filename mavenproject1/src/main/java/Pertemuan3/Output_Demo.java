9409968baa75e9da3ac5f5a981d6ed8b51bd05f1
A-22 <A-22@A-22>
2024-04-29 10:56:43 +0700

    add pertemuan 3 & 4

---
+++ mavenproject1/src/main/java/Pertemuan3/Output_Demo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan3;
+
+/**
+ *
+ * @author A-22
+ */
+public class Output_Demo {
+
+    public static void main(String[] args) {
+// Inisialisasi data
+        char huruf = 'a';
+        boolean isMasihBelajar = true;
+        int nilaiUjian = 80;
+        double hargaBarang = 370000.0;
+        float diskon = 0.1f;
+        byte umur = 20;
+        short kodePos = 6551;
+        long populasiNegara = 273523615;
+        String namaLengkap = "Agung";
+// Menampilkan data
+        System.out.println("Data:");
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Masih belajar: " + isMasihBelajar);
+        System.out.println("Nilai ujian: " + nilaiUjian);
+        System.out.println("Harga barang: " + hargaBarang);
+        System.out.println("Diskon: " + diskon);
+        System.out.println("Umur: " + umur);
+        System.out.println("Kode pos: " + kodePos);
+        System.out.println("Populasi negara: " + populasiNegara);
+        System.out.println("Nama lengkap: " + namaLengkap);
+    }
+}
